04afc77b84193614fd4bbe687cb71cbf8fd0670d
OXxJimmyxXO <95414975+Beeeeeeeluga@users.noreply.github.com>
2026-08-23 02:19:03 +0800

    Add music settings for testing zone

---
+++ Jimmy/citymusic/musicsetting.js
@@ -0,0 +1,15 @@
+export const MUSIC_ZONES = [
+    {
+        id: "testingzone",
+        name: "測試範圍",
+        dimension: "minecraft:overworld",
+        minX: 1, maxX: 2,
+        minY: 0,   maxY: 150, 
+        minZ: -1,  maxZ: 12,
+        tracks: [
+            { soundId: "custom.music.bedrock1", duration: 180 }, 
+            { soundId: "custom.music.xhakyialk", duration: 120 }, 
+            { soundId: "custom.music.helloworld", duration: 125 }  
+        ]
+    }
+];
